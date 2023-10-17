af0b10ef92da178bbe408689789ed3713bb484e6
louisa <82815171+v1peridae@users.noreply.github.com>
2023-10-17 03:21:36 -0700

    Update question-6.py
    Link to Repl: https://replit.com/@viperidae/Question-68and9#q9.py

--- question-6.py
+++ question-6.py
@@ -1,2 +1,2 @@
 price = input("Enter the item price in Kenyan Shillings >>> ")
-print("The price of the item is " , price , " Kenyan Shillings.")
+print("The price of the item is " , price , " Kenyan Shillings.") 
